a3b1db88ef3a3e64199eec57eb069d11d36675d1
ktk811 <kartik081105@gmail.com>
2026-02-01 03:36:46 +0530

    fix: allow render host in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        allowedHosts: [
+            'smurfing-hunter-frontend-grav-1.onrender.com',
+            'smurfing-hunter-frontend-grav.onrender.com'
+        ]
+    }
 })
